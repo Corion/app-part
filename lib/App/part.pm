699cd6f92cbb4c0a28344c83452febc71e549fc3
Max Maischein <corion@corion.net>
2011-02-09 20:55:20 +0100

    Import into git from http://perlmonks.org/?node_id=598718

---
+++ lib/App/part.pm
@@ -0,0 +1,13 @@
+package App::part;
+
+=head1 NAME
+
+App::part - split up files according to column value
+
+=head1 SYNOPSIS
+
+This module is just a placeholder for the main program, L<part>
+
+=cut
+
+1;
